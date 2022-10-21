c8d187591f61afd5d92ebf21bf398b9edaf6cc59
Marlon Rios <marlonrios1994@gmail.com>
2022-10-21 18:13:18 -0500

    update 1

---
+++ main.js
@@ -0,0 +1,16 @@
+// variables
+
+// nombre = "Oscar";
+// let num = 10;
+// const num2 = 100;
+
+
+// //console.log(nombre + ' ' + num + ' ' + num2);
+// console.log(`${nombre} ${num} ${num2}`);
+
+// sumar dos numeros
+
+num1 = 5;
+num2 = 10;
+
+console.log(`resultado ${num1+num2} `);
